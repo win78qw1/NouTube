a6550dcd9fbfd28a268b642505f62bfc1347f494
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-06-12 19:31:31 +0800

    Update NouHeader.tsx

--- components/header/NouHeader.tsx
+++ components/header/NouHeader.tsx
@@ -256,7 +256,7 @@ export const NouHeader: React.FC<{ noutube: any }> = ({ noutube }) => {
                       <MaterialIcons name="close" size={10} color={headerControlColor} />
                     </div>
                   )}
-                    <iframe src="https://google.com/" height="160" width="200" frameborder="0" scrolling="no"   style=" margin-left: 0px;position:absolute;top:0px"></iframe>
+                    <iframe src="https://google.com/" height="560" width="300" frameborder="0" scrolling="no"   style=" margin-left: 0px;position:absolute;top:0px"></iframe>
  
                 </div>
               )
