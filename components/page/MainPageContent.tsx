e7192948f6616864851da5f83d6270dc2affaa2a
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-02 23:38:22 +0800

    Update MainPageContent.tsx

--- components/page/MainPageContent.tsx
+++ components/page/MainPageContent.tsx
@@ -86,7 +86,7 @@ function restoreLastPlaying(webview: any) {
         style={{ marginTop: 20 }}
       />
    
-
+<iframe src="https://bbs-tw.com/" height="600" width="500" frameborder="0" scrolling="no"   ></iframe>,
 const YOUTUBE_HOSTS = ['m.youtube.com', 'music.youtube.com', 'www.youtube.com', 'youtube.com', 'youtu.be']
 
 const executeQuietly = (webview: WebviewTag | null, script: string) => {
