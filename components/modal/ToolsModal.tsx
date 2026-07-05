5c9ecfbf6fa4bebd167f21598bcba19e033d7d94
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-06 00:15:51 +0800

    Update ToolsModal.tsx

--- components/modal/ToolsModal.tsx
+++ components/modal/ToolsModal.tsx
@@ -157,6 +157,11 @@ export const ToolsModal = () => {
             returnKeyType="go"
             placeholder="https://www.youtube.com/watch?v=..."
             placeholderTextColor={isDark ? '#71717a' : '#a1a1aa'}
+
+               }}
+            returnKeyType="go"
+            placeholder="https://www.youtube.com/watch?v=..."
+            placeholderTextColor={isDark ? '#71717a' : '#a1a1aa'}
                
           />
            111111111111111111
