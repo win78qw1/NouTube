089425e8661fb8f80e7c660471a924292c65c67d
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-05 15:15:52 +0800

    Update ToolsModal.tsx

--- components/modal/ToolsModal.tsx
+++ components/modal/ToolsModal.tsx
@@ -139,6 +139,7 @@ export const ToolsModal = () => {
         <View className="gap-1">
          [ 
           <NouText className="text-sm font-semibold text-zinc-500 dark:text-zinc-400">URL</NouText>
+          111111111111111111111111111111111111
              <TextInput
             className="rounded border border-zinc-300 dark:border-zinc-700 bg-white dark:bg-zinc-900 p-2 text-sm text-zinc-900 dark:text-zinc-100"
             value={url}
