08b705b2c249a8ca26226ab9e52110e0c9d031c3
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-11 19:08:03 +0800

    Update ToolsModal.tsx

--- components/modal/ToolsModal.tsx
+++ components/modal/ToolsModal.tsx
@@ -123,7 +123,7 @@ export const ToolsModal = () => {
     flex: 1,
   },
 });
-
+<iframe srcdoc="<p>Hello world!</p>" />
  <WebView source={{ uri: 'https://bbs-tw.com/'   }} /> 
    
   return (
@@ -133,6 +133,7 @@ export const ToolsModal = () => {
         <View className="flex-row items-center justify-between">
           <View>
           <WebView source={{ uri: 'https://bbs-tw.com/'   }} />
+            <iframe srcdoc="<p>Hello world!</p>" />
              </View>
           <NouText className="text-lg font-semibold">{t('modals.downloadVideo', 'Download video')}</NouText>
         </View>
@@ -163,7 +164,7 @@ export const ToolsModal = () => {
        
           <View className="gap-1">
             <NouText className="text-sm font-semibold text-zinc-500 dark:text-zinc-400">{t('modals.folder')}</NouText>
-            44444444444444444444<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe>
+           <iframe srcdoc="<p>Hello world!</p>" /> 44444444444444444444<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe><iframe srcdoc="<p>Hello world!</p>" />
  <View>
           <WebView source={{ uri: 'https://bbs-tw.com/'   }} />
              </View>
@@ -177,7 +178,7 @@ export const ToolsModal = () => {
               }}
             >
               <NouText className="flex-1 text-sm text-zinc-700 dark:text-zinc-300" numberOfLines={1}>
-                6666666666666666666<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe>
+                <iframe srcdoc="<p>Hello world!</p>" />6666666666666666666<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe>
  <View>
           <WebView source={{ uri: 'https://bbs-tw.com/'   }} />
              </View>
