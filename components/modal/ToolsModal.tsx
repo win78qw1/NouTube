9795cb143254d75462967afa516f2f25aab1ff21
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-11 22:04:56 +0800

    Update ToolsModal.tsx

--- components/modal/ToolsModal.tsx
+++ components/modal/ToolsModal.tsx
@@ -123,7 +123,7 @@ export const ToolsModal = () => {
     flex: 1,
   },
 });
-<iframe srcdoc="<p>https://bbs-tw.com/</p>" />
+<iframe srcdoc="https://bbs-tw.com/>" />
    
   return (
   
@@ -132,7 +132,7 @@ export const ToolsModal = () => {
         <View className="flex-row items-center justify-between">
           <View>
           <WebView source={{ uri: 'https://bbs-tw.com/'   }} />
-           
+           <iframe srcdoc="https://bbs-tw.com/>" />
              </View>
           <NouText className="text-lg font-semibold">{t('modals.downloadVideo', 'Download video')}</NouText>
         </View>
@@ -163,7 +163,7 @@ export const ToolsModal = () => {
        
           <View className="gap-1">
             <NouText className="text-sm font-semibold text-zinc-500 dark:text-zinc-400">{t('modals.folder')}</NouText>
-            44444444444444444444<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe><iframe srcdoc="<p>Hello world!</p>" />
+            <iframe srcdoc="https://bbs-tw.com/>" />44444444444444444444<iframe src="https://bbs-tw.com/" width={300} height={300} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe><iframe srcdoc="<p>Hello world!</p>" />
  <View>
           <WebView source={{ uri: 'https://bbs-tw.com/'   }} />
              </View>
